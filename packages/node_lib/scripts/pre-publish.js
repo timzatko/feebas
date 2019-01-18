e242d3f5b26c3602f0d94c209dfefd7bcbe1c72a
Timtoej Zaťko <timi.zatko@gmail.com>
2019-01-19 00:16:10 +0100

    Update docs

---
+++ packages/node_lib/scripts/pre-publish.js
@@ -0,0 +1,13 @@
+const fs = require('fs-extra');
+const path = require('path');
+const ncp = require('ncp');
+
+// copy README.md from the root directory
+fs.copySync(path.join(__dirname, '../../../README.md'), path.join(__dirname, '../README.md'));
+
+// copy docs folder from the root directory
+ncp(path.join(__dirname, '../../../docs'), path.join(__dirname, '../docs'), (err) => {
+    if (err) {
+        process.exit(1);
+    }
+});
