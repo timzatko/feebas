2dfb92c18f52a13637e9b35ed84c089aa5ae1088
Timotej Zaťko <timi.zatko@gmail.com>
2020-06-19 22:44:45 +0200

    migrate to eslint

---
+++ packages/desktop_app/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 3
+    },
+    "env": {
+        "browser": true
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "extends": "plugin:@typescript-eslint/recommended"
+}
