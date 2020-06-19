cec8e1e42c1d57863b4794252d7d6351b8e2468a
Timotej Zaťko <timi.zatko@gmail.com>
2020-06-19 23:24:04 +0200

    add eslint to node_lib

---
+++ packages/node_lib/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "env": {
+        "node": true
+    },
+    "rules": {
+        "semi": "error"
+    }
+};
