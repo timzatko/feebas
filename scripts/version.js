a53e011af40086d31345ce80f5bddbf4b7533ffa
Timotej Zaťko <timi.zatko@gmail.com>
2020-06-20 10:33:15 +0200

    run prettier on version bump

--- scripts/version.js
+++ scripts/version.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
-console.log(process.argv);
+const child_process = require('child_process');
 
 function read(path) {
     return JSON.parse(fs.readFileSync(path, { flag: 'r' }).toString('utf-8'));
@@ -11,14 +10,20 @@ function write(path, data) {
     fs.writeFileSync(path, JSON.stringify(data));
 }
 
-const files = ['lerna.json', 'packages/desktop_app/package.json', 'packages/node_lib/package.json'];
+const files = ['lerna.json', 'packages/desktop_app/package.json', 'packages/node_lib/package.json'].map(file =>
+    path.join(__dirname, '..', file)
+);
 
 const { version } = read(path.join(__dirname, '..', 'package.json'));
 
-files
-    .map(file => path.join(__dirname, '..', file))
-    .forEach(filePath => {
-        const data = read(filePath);
-        data.version = version;
-        write(filePath, data);
-    });
+files.forEach(filePath => {
+    const data = read(filePath);
+    data.version = version;
+    write(filePath, data);
+});
+
+child_process.exec(`cd ${path.join(__dirname, '..')} && npx prettier ${files.join(' ')} --write && git add .`, err => {
+    if (err) {
+        throw err;
+    }
+});
